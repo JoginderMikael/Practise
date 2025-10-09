6e47fa28bb796ed5deccc9d16ad1f90df251e4e4
Joginder Mikael <jogindermikael@gmail.com>
2025-10-09 19:58:12 +0300

    Current Account implementation

--- Practise/src/bankAccountSystem/Main.java
+++ Practise/src/bankAccountSystem/Main.java
@@ -11,38 +11,7 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		Main main = new Main(); //instance to access fields
-
-
 		
-		
-		/*= new SavingsAccount("Jackline Boss", 2000);
-		CurrentAcount currentAccount = new CurrentAcount("Joginder Mikael", 1500);
-		
-	
-		//Showing account details
-		IO.println("Current Account Details");
-		currentAccount.displayDetails();
-		IO.println("Saving Account Details");
-		savingAccount.displayDetails();
-		
-		//Depositing money
-		currentAccount.deposit(1000);
-		currentAccount.displayDetails();
-		savingAccount.deposit(200);
-		savingAccount.displayDetails();
-		
-		
-		// withdrawing
-		currentAccount.withdraw(1500);
-		currentAccount.displayDetails();
-		savingAccount.withdraw(1000);
-		savingAccount.displayDetails();
-		
-		//applying interest
-		savingAccount.applyInterest();
-		savingAccount.displayDetails();
-		
-		*/
 		//BANK ACOUNT IMPLEMENTATION
 		
 		boolean exit = false;
@@ -77,7 +46,8 @@ public class Main {
 				IO.println("INVALID INPUT, PLEASE ENTER AN INTEGER (0 or 1)");
 			}
 			
-						if(scanner.nextInt() == 1) {
+					
+					if(scanner.nextInt() == 1) {
 							scanner.nextLine();
 							main.createSavingsAccount(scanner);
 
@@ -93,14 +63,19 @@ public class Main {
 							switch(menuOption) {
 									case 1 -> {
 										IO.println("Enter Amount to withdraw : ");
+										double amount;
 										
 										try {
 										
-											double amount = scanner.nextDouble();
+											amount = scanner.nextDouble();
+											scanner.nextLine();
+											
 											main.savingAccount.withdraw(amount);
 											
 										}catch (InputMismatchException e){
 											IO.println("Invalid Amount");
+											scanner.nextLine();
+											
 										}
 										
 										
@@ -122,13 +97,58 @@ public class Main {
 							}
 				
 						} else if(scanner.nextInt() == 2) {
+							
 							scanner.nextLine();
-							CreateCurrentAccount(scanner);
+							main.CreateCurrentAccount(scanner);
+							
+							IO.println("\nCURRENT ACCOUNT MENU:");
+		                    IO.println("1. Withdraw");
+		                    IO.println("2. Display Account Details");
+		                    IO.println("3. Exit to Main Menu");
+		                    IO.print("Choose Option (1-3): ");
+		                    
+		                    int menuOption;
+		                    
+		                    try {
+		                    	menuOption = scanner.nextInt();
+		                    	scanner.nextLine();
+		                    	
+		                    }catch(InputMismatchException e) {
+		                    	IO.println("INVALID INPUT");
+		                    	scanner.nextLine();
+		                    	continue;
+		                    }
+		                    
+		                    switch(menuOption) {
+				                    case 1 -> {
+				                    	
+				                    try {
+				                    	double amount = scanner.nextDouble();
+				                    	scanner.nextLine();
+				                    	main.currentAccount.withdraw(amount);
+				                    } catch(InputMismatchException e) {
+				                    	IO.println("Invalid Amount. Please enter a number");
+				                    	scanner.nextLine();
+				                    }
+				                    
+				                    }
+				                    
+				                    case 2 -> {
+				                    	main.currentAccount.displayDetails();
+				                    }
+				                    
+				                    case 3 -> {
+				                    	exit = true;
+				                    }
+				                    
+				                    default -> IO.println("INVALID INPUT");
+		                    }
 							
 						}else {
 							IO.print("Invalid Input");
 						}
 			
+		exit = true;
 		}
 
 	scanner.close();
@@ -171,8 +191,41 @@ public class Main {
 		
 		
 	}
-	static void CreateCurrentAccount(Scanner scanner) {
+	
+	private void CreateCurrentAccount(Scanner scanner) {
 		
+		boolean isInValid = true;
+		String name = null;
+		double balance;
+		
+		while(isInValid) {
+			//IO.println(isInValid);
+			IO.println("\nCURRENT ACCOUNT CREATION MENU.");
+			System.out.print("Enter Your full Names:");
+			
+			if(scanner.hasNextLine()) {
+				isInValid = false;
+				name = scanner.nextLine();
+
+			}else {
+				IO.println("Please Enter a valid Name");	
+		}
+			
+
+			IO.println("Deposit initial balance: $");
+			balance = scanner.nextDouble();
+			
+			if(balance <= 0 ) {
+				balance = 0;
+			}
+			
+			this.currentAccount = new CurrentAcount(name, balance);
+			
+			IO.println("ACCOUNT CREATED SUCCESSFULLY.");
+			savingAccount.displayDetails();
+			IO.println();
+			
+		}
 	}
 
 }
