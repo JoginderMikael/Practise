a717b226f91d7e186e4175145207f73cf7528433
Joginder Mikael <jogindermikael@gmail.com>
2025-10-12 13:09:48 +0300

    Writing file method implementation

--- Practise/src/personalFinanceTracker/FileHandler.java
+++ Practise/src/personalFinanceTracker/FileHandler.java
@@ -1,5 +1,56 @@
 package personalFinanceTracker;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileHandler {
 
+	void writeToFile(String fileName, ArrayList<Transaction> transaction) {
+		FileWriter writer = null;
+		
+		try {
+			String type;
+			String extraField = "";
+			
+			for(Transaction t : transaction) {
+			if(t instanceof Income) {
+				type = "INCOME";
+				Income income = (Income) t;
+				extraField = income.getSource();
+				
+			}else if (t instanceof Expense){
+					type = "EXPENSE";
+					Expense expense = (Expense) t;
+					extraField = expense.getPayMethod();
+				}else {
+					type = "TRANSACTION";
+				}
+			
+			String line = type + "," +
+							t.getDate() + "," +
+							t.getAmount() + "," +
+							t.getDescription() + "," +
+							extraField + "," +
+							t.getCategory() + "\n";
+			writer.write(line);
+				
+			}
+			
+			IO.println("Transaction has been saved sucessfully. " + fileName);
+		}catch(IOException e) {
+			IO.println("ERROR while saving file. " + e.getMessage());
+		} finally {
+			try {
+				if (writer != null) writer.close();
+			}catch(IOException e) {
+				IO.println ("ERROR: " + e.getMessage());
+			}
+		}
+		}
+	
+	
+	void readFromFile(String fileName) {
+
+	}
 }
--- Practise/src/personalFinanceTracker/FinanceApp.java
+++ Practise/src/personalFinanceTracker/FinanceApp.java
@@ -6,14 +6,17 @@ public class FinanceApp {
 
 	public static void main(String[] args) {
 		
-		//Transaction t1 = new Expense(1, LocalDate.now(), 50.0, "Grocery shopping", Category.FOOD);
+	//	Transaction t1 = new Expense(1, LocalDate.now(), 50.0, "Grocery shopping", Category.FOOD);
 	//	IO.println(t1.toString());
-		Income i1 = new Income(2, LocalDate.now(), 2000.0, "Salary for June", Category.SALARY, "Company XYZ");
-		IO.println(i1.toString());
+	//	Income i1 = new Income(2, LocalDate.now(), 2000.0, "Salary for June", Category.SALARY, "Company XYZ");
+	//	IO.println(i1.toString());
 	//	Expense e1 = new Expense(3, LocalDate.now(), 100.0, "Electricity bill", Category.BILLS, "Credit Card");
 	//	IO.println(e1.toString());
 		
 		
+	//	FileHandler fileHandler = new FileHandler();
+	//	fileHandler.writeToFile("transactions.txt", t1);
+	
 	}
 
 }
