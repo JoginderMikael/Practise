2795078a010936b726f78aacdda590633c69a810
Joginder Mikael <jogindermikael@gmail.com>
2025-10-12 11:00:29 +0300

    Income implementation

--- Practise/src/personalFinanceTracker/FinanceApp.java
+++ Practise/src/personalFinanceTracker/FinanceApp.java
@@ -1,11 +1,17 @@
 package personalFinanceTracker;
 
+import java.time.LocalDate;
+
 public class FinanceApp {
 
 	public static void main(String[] args) {
 		
-		Transaction t1 = new Expense(1, java.time.LocalDate.of(2024, 6, 1), 50.0, "Grocery shopping", Category.FOOD);
-		IO.println(t1.toString());
+		//Transaction t1 = new Expense(1, LocalDate.now(), 50.0, "Grocery shopping", Category.FOOD);
+	//	IO.println(t1.toString());
+		Income i1 = new Income(2, LocalDate.now(), 2000.0, "Salary for June", Category.SALARY, "Company XYZ");
+		IO.println(i1.toString());
+		
+		
 	}
 
 }
--- Practise/src/personalFinanceTracker/Income.java
+++ Practise/src/personalFinanceTracker/Income.java
@@ -4,9 +4,22 @@ import java.time.LocalDate;
 
 public class Income extends Transaction{
 
-	Income(int ID, LocalDate date, double amount, String description, Category category) {
+	
+	String source;
+	
+	Income(int ID, LocalDate date, double amount, String description, Category category, String source) {
 		super(ID, date, amount, description, category);
-		
+		this.source = source;
 	}
-
+	
+	String getSource() {
+		return this.source;
+	}
+	
+	@Override
+	public String toString() {
+		return "INCOME\n" + super.toString() + "Source = " + this.source + "\n";
+	}
+	
 }
+
