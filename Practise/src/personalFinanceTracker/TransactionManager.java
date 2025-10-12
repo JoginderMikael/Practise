4bec57b0ee39cf95d41b07fc4d06a0afb83e3ce5
Joginder Mikael <jogindermikael@gmail.com>
2025-10-12 11:28:46 +0300

    Transaction manager implementation

--- Practise/src/personalFinanceTracker/TransactionManager.java
+++ Practise/src/personalFinanceTracker/TransactionManager.java
@@ -1,5 +1,63 @@
 package personalFinanceTracker;
 
+import java.util.ArrayList;
+
 public class TransactionManager {
 
+	ArrayList<Transaction> transactions;
+	
+	TransactionManager() {
+		this.transactions = new ArrayList<>();
+	}
+	
+	void addTransaction(Transaction transaction) {
+		this.transactions.add(transaction);
+	}
+	
+	void viewTransactions() {
+		for (Transaction transaction : this.transactions) {
+			IO.println(transaction.toString());
+		}
+	}
+	
+	void searchByDate(String date) {
+		for (Transaction transaction : this.transactions) {
+			if (transaction.getDate().toString().equals(date)) {
+				IO.println(transaction.toString());
+			}
+		}
+	}
+	
+	double getTotalIncome() {
+		double totalIncome = 0.0;
+		for (Transaction transaction : this.transactions) {
+			if (transaction instanceof Income) {
+				totalIncome += transaction.getAmount();
+			}
+		}
+		return totalIncome;
+	}
+	
+	double getTotalExpense() {
+		double totalExpense = 0.0;
+		for (Transaction transaction : this.transactions) {
+			if (transaction instanceof Expense) {
+				totalExpense += transaction.getAmount();
+			}
+		}
+		return totalExpense;
+	}
+	
+	
+	double getBalance() {
+		return this.getTotalIncome() - this.getTotalExpense();
+	}
+	
+	void saveToFile() {
+		
+	}
+	
+	void loadFromFile() {
+
+	}
 }
